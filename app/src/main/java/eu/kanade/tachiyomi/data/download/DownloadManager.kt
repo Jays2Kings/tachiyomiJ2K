c7ed17a4bca4403e8e9517be437f1bf371a8133c
CarlosEsco <cargo8005@gmail.com>
2021-06-17 21:39:10 -0400

    forgot }

--- app/src/main/java/eu/kanade/tachiyomi/data/download/DownloadManager.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/download/DownloadManager.kt
@@ -379,4 +379,4 @@ class DownloadManager(val context: Context) {
 
     fun addListener(listener: DownloadQueue.DownloadListener) = queue.addListener(listener)
     fun removeListener(listener: DownloadQueue.DownloadListener) = queue.removeListener(listener)
-
+}
